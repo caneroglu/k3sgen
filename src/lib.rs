f21f2461975099168d63bd968dca7cf9da1c4db0
Can Eroğlu <can.eroglu@outlook.com>
2023-12-11 00:13:00 +0300

    commit

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod parse;
+pub mod out;
+pub mod process;
---
+++ src/parse/model.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+use clap::{arg, Command, crate_description, crate_name};
+use colored::Colorize;
+
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None,arg_required_else_help(true),
+before_help(r"
+_     _____                          
+| | __|___ /  ___   __ _   ___  _ __  
+| |/ /  |_ \ / __| / _` | / _ \| '_ \ 
+|   <  ___) |\__ \| (_| ||  __/| | | |
+|_|\_\|____/ |___/ \__, | \___||_| |_|
+                  |___/              
+"))]
+pub struct Cli {
+    #[arg(short, long)]
+    pub name: Option<String>,
+}
+
+ 
